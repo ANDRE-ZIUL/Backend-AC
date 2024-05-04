912d0a0e86d944ec54f03d25035d791d1a52f7b8
ANDRE-ZIUL <dedelcdias@gmail.com>
2024-05-04 18:53:13 -0300

    alter database host

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   dialect: 'mysql',
-  host: process.env.DATABASE_HOST,
+  host: process.env.DATABASE_HOST_AWS,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
